3915069e1e07bd6ed8791bc6ba6041d41d70ec07
Bruno Paz <184563+brpaz@users.noreply.github.com>
2026-04-26 09:00:22 +0100

    chore: updates

--- gen-test-project.sh
+++ gen-test-project.sh
@@ -2,14 +2,14 @@
 #
 # Helper script to generate a project from the template and output the path to the generated project
 # Useful for testing the template generation.
-OUTPUT_DIR=${OUTPUT_DIR:-$(mktemp -d)}
+OUTPUT_DIR=${OUTPUT_DIR:-"$(mktemp -d)/test-project"}
 
 copier copy -r HEAD --force --trust --skip-tasks \
     -d project_name="Test Project" \
     -d project_description="A test generated project" \
     -d git_repo_slug="acme/test-project" \
     -d git_repo_url="https://github.com/acme/test-project" \
-    . "$OUTPUT_DIR/test-project"
+    . "$OUTPUT_DIR"
 
 if [ $? -ne 0 ]; then
     echo "Error: Failed to generate project from template"
@@ -18,7 +18,7 @@ fi
 
 # If context is github actions, output the path to the generated project
 if [ -n "$GITHUB_ACTIONS" ]; then
-    echo "output_dir=$tmpdir/test-project" >> $GITHUB_OUTPUT
+    echo "output_dir=$OUTPUT_DIR/" >> $GITHUB_OUTPUT
 else
-    echo "Generated project path: $OUTPUT_DIR/test-project"
+    echo "Generated project path: $OUTPUT_DIR"
 fi
